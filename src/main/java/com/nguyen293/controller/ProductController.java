9364fe53d12b97695735f22099487b520c92556a
nguyen293293 <phanvannguyen020903@gmail.com>
2023-08-01 11:42:50 +0700

    first

---
+++ src/main/java/com/nguyen293/controller/ProductController.java
@@ -0,0 +1,5 @@
+package com.nguyen293.controller;
+
+public class ProductController {
+
+}
---
+++ src/test/java/com/nguyen293/Asmjava6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyen293;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asmjava6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
